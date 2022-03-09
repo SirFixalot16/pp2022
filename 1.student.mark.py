f52371a1fa074fdb0f51f8380705f0ef279039a2
Quốc Thái Vũ <quocthaivu5@gmail.com>
2022-03-09 12:01:24 +0700

    Update 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,11 +1,30 @@
 
 
-def get_number():
+def get_num_Stu():
   num = input("Enter the number of students: ")
   return num
 
 def get_stuID():
   thisStu = ["", "", ""]
+  print("Enter student information:\n")
+  thisStu[0] = input("ID: ")
+  thisStu[1] = input("Name: ")
+  thisStu[2] = input("Date of birth: ")
+  return thisStu
+
+def get_num_Course():
+  num = input("Enter the number of courses: ")
+  return num
+
+def get_courseID():
+  thisCor = ["", ""]
+  print("Enter course information:\n")
+  thisCor[0] = input("ID: ")
+  thisCor[1] = input("Name: ")
+  return thisCor
+
+def course_select(course):
+  
   
               
 
