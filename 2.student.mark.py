206efaa1de47707d8caacdb5079e1f7201aad56e
t <jeremyraberdham@gmail.com>
2022-03-29 16:02:38 +0700

    Demo

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,26 +1,33 @@
 
 class Courses:
-    def _init_(self, id, name, num_stu, mark_list):
+    def __init__(self, id, name, num_stu):
         self.id = id
         self.name = name
         self.num_stu = num_stu
 
-    def _show_(self):
-        print ('Course {self.name} has ID {self.id}.')
+    def show(self):
+        print ("Course " + self.name + " has ID: " + self.id +  ".")
+
+    def get_info(self):
+        print("Enter course information:\n")
+        self.id = input("ID: ")
+        self.name = input("\nName: ")
+
+    def get_numstu(self):
+        self.num_stu = input("Enter number of students in class: ")
+
     
-
-
 class Student:
-    def _init_(self, id, name, dob, courses):
+    def __init__(self, id, name, dob, courses):
         self.id = id
         self.name = name
         self.dob = dob
         self.courses = courses
 
-    def _intro_(self):
-        print('Student {self.name}, ID: {self.id}, DOB: {self.dob}, attend courses: ')
+    def intro(self):
+        print ("Student " + self.name + " has ID: " + self.id +  " attends the following courses: ")
         for x in range (0, len(self.courses)):
-            print("\n", self.courses[x])
+            print(self.courses[x].name, "\n")
     
     def get_info(self):
         print("Enter student information:\n")
@@ -28,8 +35,31 @@ class Student:
         self.name = input("\nName: ")
         self.dob = input("\nDate of birth: ")
 
+    def course_list(self):
+        print("Enter student's courses':\n")
+        
+CourseList = []
+StuList = []
+
+Course0 = Courses("000", "Zero", 123)
+Course0.show()
+Cor0 = []
+Stu0 = Student("240", "Tai", "16/01/1995", Cor0)
+Cor0.append(Course0)
+Stu0.intro()
+
+CourseList.append(Cor0)
+StuList.append(Stu0)
+
+def CoursePrint():
+    print("All courses: ")
+    for x in range(0, len(CourseList)):
+        print(CourseList[x], "\n")
+
+def StuPrint():
+    print("All students: ")
+    for x in range(0, len(StuList)):
+        print(StuList[x], "\n")
 
 
-
-
-print("Hellow orld") # Quick debug code, pay no mind
+print("\nHellow orld") # Quick debug code, pay no mind
